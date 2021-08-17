8bfd81efaa1b1e9f43e3bfeaff43eb6684dcb6a3
Rabiul Mazumder <60079850+RabiulMazumder613@users.noreply.github.com>
2021-08-17 15:45:15 -0400

    Update llistQueue.py

--- llistQueue.py
+++ llistQueue.py
@@ -1,3 +1,16 @@
+# Queues are another form of linear data structure very similar to stacks.
+# The difference is queues follow the FIFO rule - First In First Out, much like real life queues,
+# Where the person gets in first, gets to leave first.
+# Quesues can be implemented with both arrays and linked lists but the array implementation is not eficient
+# Because for removing an element from the queues, which happens from the front of the array(queue),
+# the indices of the array have to be updated every time, essentially making it an O(n) operation,
+# Whereas the same operation can be done in O(1) time with linked lists.
+# Queues have enqueue and dequeue operations which correspond to the push and pop operations of stacks , 
+# only difference being dequeue removes element from the front
+# Time complexities are as follows:
+# Peek - O(1)
+# Enqueue - O(1)
+# Dequeue - o(1)
 class Node():
     def __init__(self, data, next=None):
         self.data = data # Stores the value of the node
